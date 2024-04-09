3d21ff215e60ad5592d323ac228c1f4ed8eb06fa
azrideus <mehrdadtavangar@gmail.com>
2024-04-09 15:02:28 +0300

    product demo using @codespase/core

---
+++ src/components/product/ProductCard/CardProduct.tsx
@@ -0,0 +1,73 @@
+import { Product } from '@codespase/core';
+import Image from 'next/image';
+import React from 'react';
+
+/**
+ * a single product card. mostly used in sliders
+ * @param props
+ * @returns
+ */
+export const ProductCard = (props: { product: Product }) => {
+	const pr = props.product;
+	//we use the first variation of the product to show its price
+	//should we use average price or something else...?
+	const first_variation = pr.variations[0];
+	return (
+		<div
+			className="card w-48 h-80 glass flex flex-col items-center justify-around overflow-hidden py-4 px-0 !shadow-md"
+			dir="rtl"
+		>
+			<figure className="w-48 rounded-md px-4 py-0">
+				<span className="absolute right-1 top-1">
+					{first_variation.discount_percent > 0 && (
+						<div className="badge w-10 text-[13px] text-center badge-primary ">
+							{first_variation.discount_percent}
+							{'%'}
+						</div>
+					)}
+				</span>
+				<Image
+					className="w-full rounded-xl"
+					src={`/images/product/${pr.name}.jpeg`}
+					alt="Product Image"
+					width={700}
+					height={656}
+					quality={100}
+					layout="responsive"
+				/>
+			</figure>
+			<div className="card-body px-4 py-1 gap-0">
+				<p className="text-right h-min text-md leading-5 ">{pr.short_desc}</p>
+				<div className="card-actions w-full justify-between items-center">
+					<span className="flex flex-col">
+						<button role="submit">
+							<i className="bi bi-bag-plus" />
+						</button>
+					</span>
+					<span className="felx flex-col">
+						<div className="flex gap-2 justify-center items-center">
+							<div className="flex flex-col justify-end items-end">
+								<span>
+									<p>{first_variation.useprice}</p>
+								</span>
+								<span>
+									{first_variation.discount_percent > 0 && (
+										<p className="line-through text-xs">{first_variation.price}</p>
+									)}
+								</span>
+							</div>
+							<Image
+								className=""
+								src={`/images/toman.svg`}
+								alt="Currency"
+								width={30}
+								height={30}
+								quality={100}
+							/>
+						</div>
+					</span>
+				</div>
+			</div>
+		</div>
+	);
+};
---
+++ src/components/product/SliderProduct/SliderProduct.tsx
@@ -0,0 +1,38 @@
+// SliderProduct.tsx
+import Image from 'next/image';
+import SwiperLayout from '../../Sliders/SwiperLayout';
+import { ProductCard } from '../ProductCard/CardProduct';
+import { Product } from '@codespase/core';
+
+interface ProductSliderProps {
+	data: Product[];
+	className?: string;
+	title?: string;
+}
+
+/**
+ * shows a list of products as slider with a title
+ * @param props
+ * @returns
+ */
+export default function ProductSlider(props: ProductSliderProps) {
+	return (
+		<>
+			<div className="mx-5 flex flex-col gap-2 justify-center" dir="rtl">
+				<span className="flex gap-2 items-center">
+					<i className="bi bi-circle-fill"></i>
+					<ul>
+						<li>{props.title}</li>
+					</ul>
+				</span>
+				<hr className="w-full my-2 border-gray-300" />
+			</div>
+			<SwiperLayout
+				className={props.className}
+				content={props.data.map((r: any, index: any) => {
+					return <ProductCard key={index} product={r} />;
+				})}
+			/>
+		</>
+	);
+}
