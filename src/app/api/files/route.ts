ad4876cac5e26ee797d11987ac304e6161d15a42
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-04-20 21:19:30 +0300

    fixed file upload and file get

---
+++ src/app/api/files/route.ts
@@ -0,0 +1,29 @@
+import { DBFiles } from '@azrico/nodeserver';
+
+import fs from 'fs';
+import { NextResponse } from 'next/server';
+import { array_first } from '@azrico/object';
+
+export async function GET(req: Request) {
+	const fl = await DBFiles.searchFiles({});
+	return Response.json({ data: [] });
+}
+export async function POST(req: Request) {
+	try {
+		const formData = await req.formData();
+		const file = array_first(formData.getAll('file')) as unknown as File;
+
+		if (!file) {
+			throw Error('no file found');
+		}
+
+		const uploadresult = await DBFiles.upload(file.name, file);
+		return NextResponse.json({
+			status: 'success',
+			data: uploadresult,
+			fileid: uploadresult,
+		});
+	} catch (ex) {
+		return NextResponse.json({ error: String(ex) }, { status: 400 });
+	}
+}
