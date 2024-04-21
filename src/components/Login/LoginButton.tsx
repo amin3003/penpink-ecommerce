c89a1d2037a943d831b753331cb510082480d9ae
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-04-21 21:44:08 +0300

    login dialog cursor fix

--- src/components/Login/LoginButton.tsx
+++ src/components/Login/LoginButton.tsx
@@ -3,19 +3,19 @@ import { ModelLayout } from '../ModelLayout/ModelLayout';
 import LoginDialogContent from './LoginDialogContent';
 
 export default function LoginButton(props: any) {
-  return (
-    <>
-      <button
-        className="btn-sm"
-        onClick={() => (document.getElementById('my_modal') as any).showModal()}
-      >
-        <div className="flex flex-row gap-2">
-          <i className="bi bi-person-circle text-lg"/>
-        </div>
-      </button>
-      <ModelLayout dialog={"my_modal"}>
-        <LoginDialogContent />
-      </ModelLayout>
-    </>
-  );
+	return (
+		<>
+			<button
+				className="btn-sm"
+				onClick={() => (document.getElementById('my_modal') as any).showModal()}
+			>
+				<div className="flex flex-row gap-2">
+					<i className="bi bi-person-circle text-lg" />
+				</div>
+			</button>
+			<ModelLayout dialog={'my_modal'}>
+				<LoginDialogContent />
+			</ModelLayout>
+		</>
+	);
 }
--- src/components/ModelLayout/ModelLayout.tsx
+++ src/components/ModelLayout/ModelLayout.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 export const ModelLayout = (props:any) => {
     const { dialog } = props;
   return (
-    <>
-      {/* Open the modal using document.getElementById('ID').showModal() method */}
-      <dialog id={dialog} className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">{props.children}</div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
-    </>
-  );
+		<>
+			{/* Open the modal using document.getElementById('ID').showModal() method */}
+			<dialog id={dialog} className="modal modal-bottom sm:modal-middle">
+				<div className="modal-box">{props.children}</div>
+				<form method="dialog" className="modal-backdrop !cursor-default">
+					<button className="!cursor-default">close</button>
+				</form>
+			</dialog>
+		</>
+	);
 }
