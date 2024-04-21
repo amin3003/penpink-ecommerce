b43996998131668ca360f1214b5a3f05235aa7c0
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-04-21 23:00:26 +0300

    liara deploy should work now

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -4,3 +4,8 @@ export async function GET() {
 	const data = await DBManager.find('orders', {});
 	return Response.json({ data: data });
 }
+export async function POST(req: Request) {
+	return Response.json({ data: 0 });
+}
+
+ 
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,7 +1,10 @@
-import { DBManager } from '@azrico/nodeserver';
+import { DBManager, ObjectHelper, PackageHelper } from '@azrico/nodeserver';
+import { Category } from '@codespase/core';
 
 export async function GET() {
-	await DBManager.init();
 	const data = await DBManager.find('products', {});
 	return Response.json({ data: data });
+}
+export async function POST(req: Request) {
+	return Response.json({ data: 0 });
 }
