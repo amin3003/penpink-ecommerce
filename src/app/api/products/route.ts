9d3465c568c34732599118afcc96fb458a8cd5cc
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-04-21 12:12:32 +0300

    test route3

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,7 @@
 import { DBManager } from '@azrico/nodeserver';
 
 export async function GET() {
+	await DBManager.init();
 	const data = await DBManager.find('products', {});
 	return Response.json({ data: data });
 }
