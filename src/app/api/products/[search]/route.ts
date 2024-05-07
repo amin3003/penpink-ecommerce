81c49b6ce77337f1ad006302775eb46b1448f066
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-05-07 21:32:47 +0300

    decodeURIComponent for product api routes

--- src/app/api/products/[search]/route.ts
+++ src/app/api/products/[search]/route.ts
@@ -3,13 +3,13 @@ import { array_first } from '@azrico/object';
 import { Category, Product } from '@codespase/core';
 
 export async function GET(req: Request, data: any) {
-	const search = data.params.search;
+	const search = decodeURIComponent(data.params.search);
 	const pr = Product.get_single(search);
 	return Response.json({ data: pr });
 }
 export async function POST(req: Request, data: any) {
 	const reqbody = await req.json();
-	const search = data.params.search;
+	const search = decodeURIComponent(data.params.search);
 
 	const use_id = DBId.get_id_object(reqbody._id ?? search);
 	const insertbody = await ObjectHelper.prepareObject(reqbody, Product);
