0a2c1a5a881512a6d00060c58bf2870d906eb15d
amin <aminazimi1701@gmail.com>
2024-05-17 10:44:12 +0300

    change basket

--- src/components/Basket/AddToBasketButton.tsx
+++ src/components/Basket/AddToBasketButton.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Product, ProductVariation } from '@codespase/core';
 import { gstorage, gbasket } from '@azrico/global';
-=======
-import { Product, ProductVariation, BasketItem } from '@codespase/core';
->>>>>>> ayhan
 import Image from 'next/image';
+import AddToBasketController from './AddToBasketController';
 import AzFetch, { AzNextHelper } from '@azrico/fetch';
 import { FaPlusCircle } from 'react-icons/fa';
 import { GoDash } from 'react-icons/go';
@@ -19,62 +16,71 @@ export default async function AddToBasketButton(props: {
   cart?: boolean;
   countUpp?: boolean;
 }) {
-	const { product } = props;
-	const use_variation = props.variation ?? product.variations[0];
+  const { product } = props;
+  const use_variation = props.variation ?? product.variations[0];
+  const vcode = use_variation.getVariationCode();
+  const buttonid = `btn-addbasket-${vcode}`;
 
-	/**
-	 * add to basket of current user
-	 * based on current user `uid` (uid is set on `authMiddleware`)
-	 * @returns
-	 */
-	async function addToBasket() {
-		'use server';
-		return await AzFetch.post(`@/api/basket`, {
-			product_id: product._id,
-			variation_code: use_variation.variation_code,
-			add: 1,
-		});
-	}
+  /**
+   * add to basket of current user
+   * based on current user `uid` (uid is set on `authMiddleware`)
+   * @returns
+   */
+  async function addToBasket() {
+    'use server';
+    return await AzFetch.post(`@/api/basket`, { code: vcode, add: 1 });
+  }
 
   if (!use_variation) return <>product not found</>;
 
-	const btnElement = props.small ? (
-		<button className="btn btn-circle btn-ghost">
-			<i className="bi bi-bag-plus" />
-		</button>
-	) : (
-		<button className="btn btn-md text-xs md:text-md btn-primary flex items-center w-full">
-			افزودن به سبد خرید
-		</button>
-	);
+  const btnElement = props.small ? (
+    <button className="btn btn-circle btn-ghost" id={buttonid}>
+      <i className="bi bi-bag-plus" />
+    </button>
+  ) : (
+    <button
+      className="btn btn-md text-xs md:text-md btn-primary flex items-center w-full"
+      id={buttonid}
+    >
+      افزودن به سبد خرید
+    </button>
+  );
 
-	if (!props.showprice) return <span className="flex flex-col">{btnElement}</span>;
-	return (
-		<form className="flex flex-row gap-2 flex-1" dir="rtl" action={addToBasket}>
-			{/* <AddToBasketController variation_code={vcode} /> */}
-			<span className="felx flex-col flex-1">
-				<div className="flex gap-2 items-center">
-					<div className="flex flex-col justify-end items-end">
-						<span>
-							<p>{use_variation.useprice}</p>
-						</span>
-						<span>
-							{use_variation.discount_percent > 0 && (
-								<p className="line-through text-xs">{use_variation.price}</p>
-							)}
-						</span>
-					</div>
-					<Image
-						className="size-[30px]"
-						src={`/images/toman.svg`}
-						alt=""
-						width={30}
-						height={30}
-						quality={100}
-					/>
-				</div>
-			</span>
-			<span className="flex">{btnElement}</span>
-		</form>
-	);
+  if (!props.showprice)
+    return <span className="flex flex-col">{btnElement}</span>;
+  return (
+    <form
+      key={vcode}
+      className="flex flex-row gap-2 flex-1"
+      dir="rtl"
+      action={addToBasket}
+    >
+      {/* <AddToBasketController variation_code={vcode} /> */}
+      <div className="felx !flex-row w-full">
+        <div className="flex flex-row gap-2 flex-1 items-center justify-end">
+          <div className="flex flex-1 items-center ">
+            {props.countUpp ? <CountUpp /> : <BasketBtn />}
+          </div>
+          <div className="flex flex-col justify-end items-center">
+            <span>
+              <p>{use_variation.useprice}</p>
+            </span>
+            <span>
+              {use_variation.discount_percent > 0 && (
+                <p className="line-through text-xs">{use_variation.price}</p>
+              )}
+            </span>
+          </div>
+          <Image
+            className=""
+            src={`/images/toman.svg`}
+            alt="Currency"
+            width={30}
+            height={30}
+            quality={100}
+          />
+        </div>
+      </div>
+    </form>
+  );
 }
