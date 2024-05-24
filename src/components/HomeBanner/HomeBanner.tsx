a9991a2d562c1f5d0da4b0fa8f83c82b5e7ec094
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-05-24 23:50:19 +0300

    moved banners to database

--- src/components/HomeBanner/HomeBanner.tsx
+++ src/components/HomeBanner/HomeBanner.tsx
@@ -3,11 +3,30 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { AiOutlineLeft } from 'react-icons/ai';
 import BannerLayout from '../Sliders/BannerLayout';
+import DBImage from '../Image/DBImage';
 
 const bannerData = [
-	{ name: 'nahal', rightButton: false, color: '#006939' },
-	{ name: 'owner', rightButton: true, color: '#CF2F2F' },
-	{ name: 'papco', rightButton: false, color: '#1867A9' },
+	{
+		name: 'nahal',
+		banner: '6650fbdbda18ed1c5baf0fae',
+		logo: '6650fbe3da18ed1c5baf0fb1',
+		rightButton: false,
+		color: '#006939',
+	},
+	{
+		name: 'owner',
+		banner: '6650fbe7da18ed1c5baf0fb3',
+		logo: '6650fbeeda18ed1c5baf0fb5',
+		rightButton: true,
+		color: '#CF2F2F',
+	},
+	{
+		name: 'papco',
+		banner: '6650fbf4da18ed1c5baf0fb7',
+		logo: '6650fbf8da18ed1c5baf0fb9',
+		rightButton: false,
+		color: '#1867A9',
+	},
 ];
 export default function HomeBanner() {
 	return (
@@ -15,61 +34,52 @@ export default function HomeBanner() {
 			className={'h-[200px] lg:h-[70vh] lg:my-8'}
 			content={bannerData.map((dataItem: any, index: number) => {
 				return (
-          <div
-            style={{ '--color': dataItem.color } as any}
-            className="size-full px-2 overflow-clip rounded-lg relative"
-            key={index}
-          >
-            <div
-              className={clsx(
-                `absolute text-[10px] top-[24%] flex flex-col justify-between items-center gap-6`,
-                `${dataItem.rightButton ? 'left-[50%]' : 'right-[50%]'} `
-              )}
-            >
-              <div className="w-[50%]">
-                <Image
-                  className="w-full "
-                  src={`/images/banner/${dataItem.name}-logo.svg`}
-                  alt={`Banner Image ${dataItem.name}`}
-                  height={1500}
-                  width={1000}
-                  quality={100}
-                />
-              </div>
+					<div
+						style={{ '--color': dataItem.color } as any}
+						className="size-full px-2 overflow-clip rounded-lg relative"
+						key={index}
+					>
+						<div
+							className={clsx(
+								`absolute text-[10px] top-[24%] flex flex-col justify-between items-center gap-6`,
+								`${dataItem.rightButton ? 'left-[50%]' : 'right-[50%]'} `
+							)}
+						>
+							<div className="w-[50%]">
+								<DBImage
+									className="w-full "
+									src={`${dataItem.logo ?? dataItem.name}`}
+									height={1500}
+									width={1000}
+								/>
+							</div>
 
-              <div className="justify-center items-center px-2 hidden lg:flex">
-                <b
-                  className={clsx(
-                    `text-right text-[--color] text-sm`,
-                    `text-center`
-                  )}
-                >
-                  {dataItem.name} انواع محصولات برند
-                </b>
-              </div>
-              <Link href={'#'} className="">
-                <button
-                  className={clsx(
-                    'text-sm flex justify-center items-center bg-white rounded-full px-2 py-1',
-                    'text-[--color]'
-                  )}
-                >
-                  {/* <i className="bi bi-caret-left-fill"/> */}
-                  <AiOutlineLeft />
-                  مشاهده
-                </button>
-              </Link>
-            </div>
-            <Image
-              className="size-full select-none object-cover rounded-lg"
-              src={`/images/banner/${dataItem.name}-banner.svg`}
-              alt={`banner`}
-              height={1028}
-              width={2048}
-              quality={100}
-            />
-          </div>
-        );
+							<div className="justify-center items-center px-2 hidden lg:flex">
+								<b className={clsx(`text-right text-[--color] text-sm`, `text-center`)}>
+									{dataItem.name} انواع محصولات برند
+								</b>
+							</div>
+							<Link href={'#'} className="">
+								<button
+									className={clsx(
+										'text-sm flex justify-center items-center bg-white rounded-full px-2 py-1',
+										'text-[--color]'
+									)}
+								>
+									{/* <i className="bi bi-caret-left-fill"/> */}
+									<AiOutlineLeft />
+									مشاهده
+								</button>
+							</Link>
+						</div>
+						<DBImage
+							className="size-full select-none object-cover rounded-lg"
+							src={`${dataItem.banner ?? dataItem.name}`}
+							height={1028}
+							width={2048}
+						/>
+					</div>
+				);
 			})}
 		/>
 	);
