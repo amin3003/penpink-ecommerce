850597a53c26696dccb97c1716e029df98f4667c
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-07-12 14:10:20 +0300

    not-found farsi

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,20 +3,18 @@ import Layout from './[locale]/layout';
 export default function NotFound() {
   //TODO better 404 page
   return (
-    <Layout params={{lang:"fa"}} >
-      <body>
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-          <h1 className="text-9xl font-bold text-red-500">404</h1>
-          <p className="mt-4 text-2xl text-gray-700">
-            Oops! The page you're looking for can't be found.
-          </p>
-          <Link href="/">Return Home</Link>
-          <div className="relative w-48 h-48 mt-8">
-            <div className="absolute inset-0 w-full h-full bg-red-500 rounded-full animate-bounce"></div>
-            <div className="absolute top-12 left-12 w-16 h-16 bg-white rounded-full"></div>
-          </div>
-        </div>
-      </body>
-    </Layout>
-  );
+		<Layout params={{ lang: 'fa' }}>
+			<body>
+				<div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+					<h1 className="text-9xl font-bold text-red-500">404</h1>
+					<p className="mt-4 text-2xl text-gray-700">صفحه مورد نظر پیدا نشد!</p>
+					<Link href="/">برو به خانه</Link>
+					<div className="relative w-48 h-48 mt-8">
+						<div className="absolute inset-0 w-full h-full bg-red-500 rounded-full animate-bounce"></div>
+						<div className="absolute top-12 left-12 w-16 h-16 bg-white rounded-full"></div>
+					</div>
+				</div>
+			</body>
+		</Layout>
+	);
 }
