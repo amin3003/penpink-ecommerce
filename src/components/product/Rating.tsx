5b4a8390b2e651e9d083cc25c9e51ecbbe7e3863
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-08-16 12:44:11 +0300

    default rating is 4

--- src/components/product/Rating.tsx
+++ src/components/product/Rating.tsx
@@ -10,8 +10,18 @@ export default function Rating(props: { rate?: number; className?: string }) {
 				className="mask mask-star-2 bg-orange-400"
 				defaultChecked
 			/>
-			<input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-			<input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+			<input
+				type="radio"
+				name="rating-2"
+				defaultChecked
+				className="mask mask-star-2 bg-orange-400"
+			/>
+			<input
+				type="radio"
+				name="rating-2"
+				defaultChecked
+				className="mask mask-star-2 bg-orange-400"
+			/>
 			<input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
 		</div>
 	);
